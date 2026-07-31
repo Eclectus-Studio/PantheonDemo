107656a70bce21b3834cccfb3c37556a5720104b
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-07-31 11:13:09 -0400

    initial commit!

---
+++ src/main/java/com/eclectusstudio/pantheonDemo/PantheonDemo.java
@@ -0,0 +1,16 @@
+package com.eclectusstudio.pantheonDemo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PantheonDemo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
